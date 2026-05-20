ba0459814a1c705cb819c19df239f9e1306b2a40
siberianhuskytoucan <elijahsiberianhuskyretrievertoucan99899@proton.me>
2026-05-20 20:07:22 +0000

    Describe your changes here

--- website_root/js/config.js
+++ website_root/js/config.js
@@ -2,7 +2,7 @@
 window.shopConfig = {
   siteName: 'flavors99789catalog.com',
   // Default receiving address shown at checkout. Replace with your real address.
-  defaultCryptoAddress: 'ENTER_YOUR_CRYPTO_ADDRESS_HERE',
+  defaultCryptoAddress: 'bc1qvxukeasjgkz7nzvrvk9a9er7a33rstrdv5u4e5',
   // Backend server URL (for creating hosted charges). If your server is hosted on another origin, set the full URL, e.g. 'https://payments.example.com'
   backendUrl: '',
   // Default currency to show
